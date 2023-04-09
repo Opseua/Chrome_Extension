fcd8819e4d2cbe03884a255087e7c9df09df5f83
Orlando <opseua@gmail.com>
2023-04-09 16:13:21 -0300

    Removido consoles desnecessários

--- src/recursos/Api.js
+++ src/recursos/Api.js
@@ -8,7 +8,7 @@ async function Api(inf) {
   try {
     let r = JSON.parse(`{"w": "${a}", "z": { "method": "${b}","headers":${JSON.stringify(c)}${(b == "GET") ? '' : `,"${(typeof UrlFetchApp == "undefined") ? "body" : "payload"}":"${d}"`}}}`);
     let s = (typeof UrlFetchApp == "undefined") ? await fetch(r.w, r.z) : UrlFetchApp.fetch(r.w, r.z);
-    console.log('API: OK');
+    //console.log('API: OK');
     return s.json();
   } catch (error) {
     console.log('API: DEU ERRO');
