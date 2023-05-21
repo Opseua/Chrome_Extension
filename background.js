349f7706d30b1824436165d6694fe6c55b126e54
Orlando <opseua@gmail.com>
2023-05-21 04:51:21 -0300

    Removido código desnecessário

--- background.js
+++ background.js
@@ -20,107 +20,70 @@ import('./src/recursos/Network.js').then(module => {
 
 // *******************************************************
 
-
-let socket1;
-let socket2;
-let mensagem = '';
-let socket1Conectado = false;
-let socket2Conectado = false;
-
-function exibirMensagem(texto) {
-  console.log(texto);
-}
-
+// ######################### CONEXÃO | WEBSOCKET
 async function connect() {
-  if (!socket1Conectado) {
-    socket1 = new WebSocket('wss://ntfy.sh/OPSEUA/ws');
+  let ws1;
+  let ws2;
+  let ws1On = false;
+  let ws2On = false;
+  function msg(txt) {
+    console.log(txt);
+  }
 
-    // CONEXAO: ONLINE - Socket 1
-    socket1.addEventListener('open', async function (event) {
-      exibirMensagem('BACKGROUND: CONEXÃO ESTABELECIDA - Socket 1');
-      socket1Conectado = true;
+  if (!ws1On) {
+    ws1 = new WebSocket('wss://ntfy.sh/OPSEUA/ws');
+    
+    // CONEXAO: ONLINE - WS 1
+    ws1.addEventListener('open', async function (event) {
+      msg('BACKGROUND: CONEXÃO ESTABELECIDA - WS 1');
+      ws1On = true;
     });
 
-    // CONEXAO: NOVA MENSAGEM - Socket 1
-    socket1.addEventListener('message', async function (event) {
+    // CONEXAO: NOVA MENSAGEM - WS 1
+    ws1.addEventListener('message', async function (event) {
       if (JSON.parse(event.data).event == 'message') {
-        exibirMensagem('BACKGROUND: CONEXAO NOVA MENSAGEM - Socket 1');
+        msg('BACKGROUND: CONEXAO NOVA MENSAGEM - WS 1');
         //ApiNovaInformacao(background)
       }
     });
 
-    // CONEXAO: OFFLINE, TENTAR NOVAMENTE - Socket 1
-    socket1.addEventListener('close', async function (event) {
-      exibirMensagem('BACKGROUND: RECONEXÃO EM 30 SEGUNDOS - Socket 1');
-      socket1Conectado = false;
+    // CONEXAO: OFFLINE, TENTAR NOVAMENTE - WS 1
+    ws1.addEventListener('close', async function (event) {
+      msg('BACKGROUND: RECONEXÃO EM 30 SEGUNDOS - WS 1');
+      ws1On = false;
       setTimeout(connect, 30000);
     });
   }
 
-  if (!socket2Conectado) {
-    socket2 = new WebSocket('ws://localhost:8887');
+  if (!ws2On) {
+    ws2 = new WebSocket('ws://localhost:8887');
 
-    // CONEXAO: ONLINE - Socket 2
-    socket2.addEventListener('open', async function (event) {
-      exibirMensagem('BACKGROUND: CONEXÃO ESTABELECIDA - Socket 2');
-      socket2Conectado = true;
+    // CONEXAO: ONLINE - WS 2
+    ws2.addEventListener('open', async function (event) {
+      msg('BACKGROUND: CONEXÃO ESTABELECIDA - WS 2');
+      ws2On = true;
       setTimeout(function () {
-        socket2.send("Chrome: mensagem de teste");
+        ws2.send("Chrome: mensagem de teste");
       }, 3000);
     });
 
-    // CONEXAO: NOVA MENSAGEM - Socket 2
-    socket2.addEventListener('message', async function (event) {
-      exibirMensagem('BACKGROUND: CONEXAO NOVA MENSAGEM - Socket 2');
-      exibirMensagem('→ ' + event.data);
+    // CONEXAO: NOVA MENSAGEM - WS 2
+    ws2.addEventListener('message', async function (event) {
+      msg('BACKGROUND: CONEXAO NOVA MENSAGEM - WS 2');
+      msg('→ ' + event.data);
     });
 
-    // CONEXAO: OFFLINE, TENTAR NOVAMENTE - Socket 2
-    socket2.addEventListener('close', async function (event) {
-      exibirMensagem('BACKGROUND: RECONEXÃO EM 30 SEGUNDOS - Socket 2');
-      socket2Conectado = false;
+    // CONEXAO: OFFLINE, TENTAR NOVAMENTE - WS 2
+    ws2.addEventListener('close', async function (event) {
+      msg('BACKGROUND: RECONEXÃO EM 30 SEGUNDOS - WS 2');
+      ws2On = false;
       setTimeout(connect, 30000);
     });
   }
+
 }
 
 connect();
-
-
-
-
-
-
-
-chamar()
-
-// ######################### CONEXÃO | WEBSOCKET
-/* let socket;
-async function connect() {
-  socket = new WebSocket('wss://ntfy.sh/OPSEUA/ws');
-
-  // CONEXAO: ONLINE
-  socket.addEventListener('open', async function (event) {
-    console.log('BACKGROUND: CONEXAO ESTABELECIDA');
-    VariavelGlobal();
-  });
-
-  // CONEXAO: NOVA MENSAGEM
-  socket.addEventListener('message', async function (event) {
-    const background = JSON.parse(event.data)
-    if (background.event == 'message') {
-      console.log('BACKGROUND: CONEXAO NOVA MENSAGEM');
-      ApiNovaInformacao(background)
-    }
-  });
-
-  // CONEXAO: OFFLINE, TENTAR NOVAMENTE
-  socket.addEventListener('close', async function (event) {
-    console.log('BACKGROUND: RECONEXAO EM 30 SEGUNDOS');
-    setTimeout(connect, 30000);
-  });
-}
-connect(); */
 
 // ######################### CLICK NO ICONE
 chrome.browserAction.onClicked.addListener(async function () {
@@ -129,9 +92,6 @@ chrome.browserAction.onClicked.addListener(async function () {
 
 // ######################### ATALHO PRESSIONADO
 chrome.commands.onCommand.addListener(async function (command) {
-
-
-
 
   // identificar comando do atalho
   function getShortcutForCommand(commandName, callback) {
@@ -161,338 +121,5 @@ chrome.commands.onCommand.addListener(async function (command) {
   AtalhoPressionado(atalho_comando);
 
 });
-
-
-
-
-function chamar() {
-
-  console.log("OK")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  /* 
-  
-  
-  
-  
-  // Criar uma função para interceptar e modificar a requisição
-  function interceptarRequisicao(details) {
-    // Extrair as informações relevantes da requisição
-    const { url, method, requestHeaders } = details;
-  
-    // Exibir as informações da requisição original no console
-    console.log('Requisição original:');
-    console.log('URL:', url);
-    console.log('Método:', method);
-    console.log('Cabeçalhos:', requestHeaders);
-  
-    // Cancelar a requisição original
-    return { cancel: true };
-  }
-  
-  // Adicionar um listener para o evento 'onBeforeRequest' da API 'webRequest'
-  chrome.webRequest.onBeforeRequest.addListener(
-    interceptarRequisicao,
-    { urls: ["<all_urls>"] },
-    ["requestHeaders", "blocking"]
-  );
-  
-  
-  
-  
-  
-  
-  
-  
-    return
-  
-  
-  
-  
-    let requestInfo = {};
-    let onBeforeRequestListener;
-    let onBeforeSendHeadersListener;
-  
-    function iniciarMonitoramento() {
-  
-      onBeforeRequestListener = function (details) {
-        var tipo = "";
-        if (details.type === "xmlhttprequest") {
-          let requestBody = "";
-          if (details.requestBody) {
-            if (details.requestBody.formData) { // BODY TIPO: formData
-              requestBody = JSON.stringify(details.requestBody.formData);
-              tipo = "formData";
-            }
-            else if (details.requestBody.raw) { // BODY TIPO: raw
-              const rawBytes = new Uint8Array(details.requestBody.raw[0].bytes);
-              requestBody = String.fromCharCode.apply(null, rawBytes);
-              tipo = "raw";
-            }
-            else if (details.requestBody.urlencoded) { // BODY TIPO: urlencoded
-              const params = new URLSearchParams();
-              for (const key in details.requestBody.urlencoded) {
-                if (details.requestBody.urlencoded.hasOwnProperty(key)) {
-                  params.append(key, details.requestBody.urlencoded[key]);
-                }
-              }
-              requestBody = params.toString();
-              tipo = "urlencoded";
-            }
-          }
-          requestInfo = {
-            body: requestBody,
-            tipo: tipo,
-            documentId: details.documentId
-          };
-        }
-      };
-  
-      onBeforeSendHeadersListener = function (details) {
-        requestInfo = {
-          x: "ETAPA: 2",
-          method: details.method,
-          url: details.url,
-          headers: details.requestHeaders,
-          ...requestInfo,
-        };
-        console.log(requestInfo);
-        if (requestInfo.body.includes("CASA")) {
-          console.log("REQUISICAO CANCELADA");
-          return { cancel: true };
-        } else {
-          console.log("REQUISICAO ENVIADA");
-          return { cancel: false };
-        }
-      };
-  
-      chrome.webRequest.onBeforeRequest.addListener(
-        onBeforeRequestListener,
-        { urls: ["<all_urls>"] },
-        ["requestBody"]
-      );
-  
-      chrome.webRequest.onBeforeSendHeaders.addListener(
-        onBeforeSendHeadersListener,
-        { urls: ["<all_urls>"] },
-        ["requestHeaders", "blocking"]
-      );
-    }
-  
-    function pararMonitoramento() {
-      chrome.webRequest.onBeforeRequest.removeListener(onBeforeRequestListener);
-      chrome.webRequest.onBeforeSendHeaders.removeListener(onBeforeSendHeadersListener);
-    }
-  
-    function alterarMonitoramento(valor) {
-      if (valor) {
-        iniciarMonitoramento();
-      } else {
-        console.log("REMOVENDO LISTENER");
-        pararMonitoramento();
-      }
-    }
-  
-    async function concluido(inf) {
-  
-      let requisicaoCancelar = '';
-      let listenerRemover = '';
-      console.log(inf);
-  
-      if (inf.body.includes("CASA")) {
-        requisicaoCancelar = true
-        listenerRemover = true
-      } else {
-        requisicaoCancelar = false
-        listenerRemover = false
-      }
-  
-      return requisicaoCancelar
-  
-    }
-  
-    alterarMonitoramento(true);
-  
-  
-  
-  
-  
-  
-  
-   */
-
-
-
-
-
-
-
-
-
-
-
-
-  /* TUDO OK
-  
-  
-  let requestInfo = {};
-    chrome.webRequest.onBeforeRequest.addListener(
-      async function (details) {
-        var tipo = "";
-        if (details.type === "xmlhttprequest") {
-          let requestBody = '';
-          if (details.requestBody) {
-            if (details.requestBody.formData) { // BODY TIPO: formData
-              requestBody = JSON.stringify(details.requestBody.formData);
-              tipo = "formData";
-            }
-            else if (details.requestBody.raw) { // BODY TIPO: raw
-              const rawBytes = new Uint8Array(details.requestBody.raw[0].bytes);
-              requestBody = String.fromCharCode.apply(null, rawBytes);
-              tipo = "raw";
-            }
-            else if (details.requestBody.urlencoded) { // BODY TIPO: urlencoded
-              const params = new URLSearchParams();
-              for (const key in details.requestBody.urlencoded) {
-                if (details.requestBody.urlencoded.hasOwnProperty(key)) {
-                  params.append(key, details.requestBody.urlencoded[key]);
-                }
-              }
-              requestBody = params.toString();
-              tipo = "urlencoded";
-            }
-          }
-          requestInfo = {
-            x: "ETAPA: 1",
-            url: details.url,
-            method: details.method,
-            body: requestBody,
-            tipo: tipo,
-          };
-          if (requestInfo.headers) {
-            displayRequestInfo();
-          }
-        }
-      },
-      { urls: ["<all_urls>"] },
-      ["requestBody"]
-    );
-    
-    chrome.webRequest.onBeforeSendHeaders.addListener(
-      function (details) {
-        const method = details.method;
-        const url = details.url;
-        const headers = details.requestHeaders;
-        // Adiciona o novo cabeçalho "TESTE: VALOR"
-        headers.push({ name: 'TESTE', value: 'VALOR' });
-        requestInfo = {
-          ...requestInfo,
-          x: "ETAPA: 2",
-          url: url,
-          method: method,
-          headers: headers,
-        };
-        if (requestInfo.body) {
-          displayRequestInfo();
-        }
-        return { requestHeaders: headers };
-      },
-      { urls: ["<all_urls>"] },
-      ["requestHeaders", "blocking"]
-    );
-    
-    // Função para exibir as informações no console
-    function displayRequestInfo() {
-      console.log(requestInfo);
-    }
-  
-  
-  
-  
-  
-  
-  /*
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-    /* function injectScript(tabId) {
-      chrome.tabs.executeScript(tabId, {
-  
-          const origWebSocket : WebSocket;
-          const newWebSocket = function(url, protocols) {
-            console.log("WebSocket conectado em", url);
-            const socket = new origWebSocket(url, protocols);
-            socket.addEventListener("message", function(event) {
-              console.log("Mensagem recebida:", event.data);
-            });
-            return socket;
-          };
-          WebSocket = newWebSocket;
-        
-      });
-    }
-    
-  */
-
-
-
-
-
-
-
-
-
-
-
-}
 
 
