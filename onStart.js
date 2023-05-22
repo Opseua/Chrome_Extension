2faff69037f7cddfb1c7150082dbe4fe2111c35d
Orlando <opseua@gmail.com>
2023-05-22 08:29:51 -0300

    Adicionado checagem 'NodeJS' ou 'Chrome'

--- onStart.js
+++ onStart.js
@@ -1,7 +1,20 @@
+if (typeof process !== 'undefined' && process.release && process.release.name === 'node') {
+    // RODANDO NO NODE
+    onStart()
+}
+
+// ############################################################################
+
 async function onStart(inf) {
 
-    console.log("onStart a")
+    console.log("onStart")
 
 }
 
-export default onStart 
+export default onStart
+
+
+
+
+
+
