5c106a3ab406039fd7ead7bd01e7f5d2146bd67a
Orlando <opseua@gmail.com>
2023-06-26 08:30:09 -0300

    Função 'nodeOrBrowser' corrigida

--- src/resources/nodeOrBrowser.js
+++ src/resources/nodeOrBrowser.js
@@ -1,10 +1,24 @@
 async function nodeOrBrowser() {
+    let ret = { ret: false }
 
-    if (typeof window === 'undefined') { // NODE
-        return { ret: true, res: 'node' }
-    } else { // BROWSER
-        return { ret: true, res: 'browser' }
-    }
+    try {
+        if (typeof process.versions.node !== 'undefined') {
+            ret['ret'] = true; ret['res'] = 'NodeJS'
+        }
+    } catch { }
+
+    try {
+        if (typeof window !== 'undefined') {
+            ret['ret'] = true; ret['res'] = 'Chrome'
+        }
+    } catch { }
+
+    try {
+        if (typeof UrlFetchApp !== 'undefined') {
+            ret['ret'] = true; ret['res'] = 'Google App Script'
+        }
+    } catch { }
+
 }
 
 export { nodeOrBrowser }
