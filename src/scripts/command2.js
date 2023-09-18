330f708fa88f75dfefc2440416c787e9297ff223
Orlando <opseua@gmail.com>
2023-09-18 07:15:03 -0300

    Links → 'arrUrl'

--- src/scripts/command2.js
+++ src/scripts/command2.js
@@ -2,26 +2,20 @@ async function command2(inf) {
   let ret = { 'ret': false }
   try {
     let reRun = false
-    let infSniffer, retSniffer
+    let infSniffer, retSniffer, infConfigStorage, retConfigStorage
+    infConfigStorage = { 'action': 'get', 'key': 'sniffer' }; retConfigStorage = await configStorage(infConfigStorage)
+    if (!retConfigStorage.ret) { return } else { retConfigStorage = retConfigStorage.res }; const arrUrl = retConfigStorage.arrUrl
     const gOEve = async (i) => {
       if (i.inf.sniffer === 2) { gORem(gOEve); chrome.browserAction.setBadgeText({ text: '' }); reRun = false; return ret }
     }; gOAdd(gOEve);
     async function run() {
       reRun = false
-      infSniffer = {
-        'newReqSend': true,
-        'arrUrl': [
-          'https://desk.oneforma.com/scribo_apps/MTPE_new_process/postediting.php*',
-          'https://www.tryrating.com/api/survey',
-          'https://rating.ewoq.google.com/u/0/rpc/rating/SafeTemplateService/GetTemplate',
-          'https://rating.ewoq.google.com/u/0/rpc/rating/AssignmentAcquisitionService/GetNewTasks'
-        ]
-      }
+      infSniffer = { 'newReqSend': true, 'arrUrl': arrUrl }
       retSniffer = await sniffer(infSniffer)
       if (!retSniffer.res || !gO.inf.sniffer == 1) { return ret }
 
       // #### OneForma
-      if (regex({ 'simple': true, 'text': retSniffer.res.res.url, 'pattern': 'https://desk.oneforma.com/scribo_apps/MTPE_new_process/postediting.php*' })) {
+      if (regex({ 'simple': true, 'text': retSniffer.res.res.url, 'pattern': arrUrl[8] })) {
         if (regex({ 'simple': true, 'text': JSON.stringify(retSniffer.res.res.body), 'pattern': '*MTPE_New_PortugueseBrazilToEnglish*' })) {
           // MTPE_New_PortugueseBrazilToEnglish
           const infOneFormaMPTE = { 'sniffer': retSniffer.res.res.body }
@@ -32,7 +26,7 @@ async function command2(inf) {
           }
         }
         // #### Peroptyx
-      } else if (regex({ 'simple': true, 'text': retSniffer.res.res.url, 'pattern': 'https://www.tryrating.com/api/survey*' })) {
+      } else if (regex({ 'simple': true, 'text': retSniffer.res.res.url, 'pattern': arrUrl[6] })) {
         if (regex({ 'simple': true, 'text': JSON.stringify(retSniffer.res.res.body), 'pattern': '*Search 2.0*' })) {
           // Search 2.0
           const infPeroptyx_Search20 = { 'sniffer': retSniffer.res.res.body }
@@ -61,5 +55,5 @@ async function command2(inf) {
 if (typeof window !== 'undefined') { // CHROME
   window['command2'] = command2;
 } else { // NODEJS
- // global['command2'] = command2;
+  // global['command2'] = command2;
 }
