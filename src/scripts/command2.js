57eff37cdaa7910d8b2c2345d3cc7421ba09d3b6
Orlando <opseua@gmail.com>
2024-06-23 06:34:48 -0300

    Correção '!letter!' do 'commandLine'

--- src/scripts/command2.js
+++ src/scripts/command2.js
@@ -9,7 +9,7 @@ async function command2(inf) {
     }
     let retNotification = await notification(infNotification);
 
-    let infCommandLine = { 'e': e, 'command': `${letter}:/ARQUIVOS/PROJETOS/Sniffer_Python/src/z_Outros_server/TOGGLE_HIDE.vbs`, 'retInf': true }
+    let infCommandLine = { 'e': e, 'command': `!letter!:/ARQUIVOS/PROJETOS/Sniffer_Python/src/z_Outros_server/TOGGLE_HIDE.vbs`, 'retInf': true }
     let retCommandLine = await commandLine(infCommandLine)
 
     ret['ret'] = true;
